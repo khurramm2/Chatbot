0ea22db431087b8a800635e059ae26efb1a868df
khurramm2 <khurramm2@uni.coventry.ac.uk>
2022-11-14 06:52:58 +0000

    w

---
+++ responses.py
@@ -0,0 +1,9 @@
+response_a = "I am an expert at football"
+response_b = "I hate Manchester City"
+response_c = "yeah I like {}"
+response_d = "my favorite team is Manchester United"
+response_e = "I've been watching football since I was two"
+blank_spot = "player"
+
+
+responses = [response_a, response_b, response_c, response_d, response_e]
