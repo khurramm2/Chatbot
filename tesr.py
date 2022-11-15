017320e33a85e60c817bd2326ab97ef0e64282bb
khurramm2 <khurramm2@uni.coventry.ac.uk>
2022-11-15 16:06:40 +0000

    1

--- tesr.py
+++
@@ -1,9 +0,0 @@
-import re
-from collections import Counter
-import spacy
-word2vec = spacy.load("en_core_web_sm")
-from nltk import pos_tag
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-stop_words = set(stopwords.words("english"))
-
